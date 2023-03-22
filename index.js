51c07ba0132ed2a41dabff2ee7b61223835e2b99
Antwoine Griggs <97367683+antwoinegriggs@users.noreply.github.com>
2023-03-22 05:49:30 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(event = "roller-skate") {
+  return `This Saturday, I want to ${event}!`;
+}
+
+function mondayWork(event = "go to the office") {
+  return `This Monday, I will ${event}.`;
+}
+
+function wrapAdjective(symbol = "*") {
+  return function newfunction(special = "special") {
+    return `You are ${symbol}${special}${symbol}!`;
+  };
+}
